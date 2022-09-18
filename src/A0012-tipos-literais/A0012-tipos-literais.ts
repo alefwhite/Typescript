b227cd7dfc3b76a36446af09f28bedb73a11d331
alefwhite <alefwhite@hotmail.com>
2022-09-18 03:02:03 -0300

    teste

---
+++ src/A0012-tipos-literais/A0012-tipos-literais.ts
@@ -0,0 +1,13 @@
+let x = 10; // eslint-disable-line
+x = 0b1010;
+const y = 10;
+const a = 100; // eslint-disable-line
+
+const pessoa = {
+  nome: 'Luiz' as const,
+  sobrenome: 'Miranda',
+};
+
+export function escolhaCor(cor: 'Vermelho' | 'Amarelo' | 'Azul'): string {
+  return cor;
+}
